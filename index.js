87f107ae8fe5221ef07b4c7df5ae76e42cfe6741
Cleber-0101 <cleberbatista99@hotmail.com>
2025-10-06 14:59:37 -0300

    Add middwares na api

--- index.js
+++ index.js
@@ -5,41 +5,64 @@ server.use(express.json());
 
 const cursos = ['node.js', 'JavaScript', 'c#', 'java', 'react native', "python"];
 
+//Middware Global
+//passa pelo middware e segue o fluxo
+server.use((req, res, next) => {
+    console.log(`Url chamada ${req.url}`)
+    return next();
+})
+
+function checandoCurso(req, res, next) {
+    if (!req.body.nome) {
+        return res.status(400).json({ error: "nome do curso é obrigatorio" })
+    }
+
+    //sem retorno não funciona
+    return next()
+}
+
+
+function checandoIndexExistente(req,res) {
+    const curso = cursos[req.params.index]
+    if (!curso) {
+        return res.status(400).json({ error: "usuario não existe" })
+    }
+}
+
 //lista todos (read)
-server.get("/curso", (req,res) => {
+server.get("/curso", checandoIndexExistente,(req, res) => {
     return res.json(cursos)
 })
 
 //lista um unico curso (read)
 server.get("/curso/:index", (req, res) => {
-    const {index} = req.params;
+    const { index } = req.params;
     return res.json(cursos[index])
 })
 
 //criando um curso pelo corpo da requisição
-server.post("/curso/criar", (req,res) =>{
+server.post("/curso/criar", checandoCurso, (req, res) => {
     const { nome } = req.body;
     cursos.push(nome)
 
     return res.json(cursos)
 })
 
-server.put("/cursos/:index", (req,res) => {
-    const {index} = req.params
-    const {name} = req.body
+server.put("/cursos/:index", checandoCurso, checandoIndexExistente,(req, res) => {
+    const { index } = req.params
+    const { nome } = req.body
 
-    cursos[index] = name;
-
+    cursos[index] = nome;
     return res.json(cursos)
 })
 
 
-server.delete("/cursos/:index", (req, res) =>{
-    const {index} = req.params
-    cursos.splice(index , 1)
+server.delete("/cursos/:index", checandoIndexExistente, (req, res) => {
+    const { index } = req.params
+    cursos.splice(index, 1)
 
     //return res.json(cursos)
-    return res.json({message : "curso deletado com sucesso"})
+    return res.json({ message: "curso deletado com sucesso" })
 
 })
 

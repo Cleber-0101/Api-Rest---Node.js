4d4e3c265236bf4f2d38fffa14deeae5570e3507
Cleber-0101 <cleberbatista99@hotmail.com>
2025-10-06 12:46:00 -0300

    Crud compleot com Node.js

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const express = require("express")
+const express = require('express')
 const server = express()
+
+server.use(express.json());
 
 const cursos = ['node.js', 'JavaScript', 'c#', 'java', 'react native', "python"];
 
-//Criando rota 
-//localhost:3000/curso
+//lista todos (read)
+server.get("/curso", (req,res) => {
+    return res.json(cursos)
+})
+
+//lista um unico curso (read)
 server.get("/curso/:index", (req, res) => {
-    // return resposta.send("hello word")
-    //Query params
-    const nome = req.query.nome;
-
-    //Route params
     const {index} = req.params;
-
-    //retornando um objeto
     return res.json(cursos[index])
 })
 
-//Ouvindo essa porta
+//criando um curso pelo corpo da requisição
+server.post("/curso/criar", (req,res) =>{
+    const { nome } = req.body;
+    cursos.push(nome)
+
+    return res.json(cursos)
+})
+
+server.put("/cursos/:index", (req,res) => {
+    const {index} = req.params
+    const {name} = req.body
+
+    cursos[index] = name;
+
+    return res.json(cursos)
+})
+
+
+server.delete("/cursos/:index", (req, res) =>{
+    const {index} = req.params
+    cursos.splice(index , 1)
+
+    //return res.json(cursos)
+    return res.json({message : "curso deletado com sucesso"})
+
+})
+
+//escutando a porta 
 server.listen(3000)
